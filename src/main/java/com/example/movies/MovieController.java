526389ba36ddf030dab41667e5a6c006b7753d21
Shivam Mahajan <spmshivam@gmail.com>
2023-05-08 01:29:15 -0400

    Created the structure to retrieve movie data

--- src/main/java/com/example/movies/MovieController.java
+++ src/main/java/com/example/movies/MovieController.java
@@ -1,5 +1,10 @@
 package com.example.movies;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public String allMovies(){
-        return "All Movies!";
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     } 
 }
