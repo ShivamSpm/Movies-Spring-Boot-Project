4c42e3866d9d8fe78afa31213b90bc1a8c4ea530
Shivam Mahajan <spmshivam@gmail.com>
2023-05-08 15:31:09 -0400

    Added a get mapping for imdbId

--- src/main/java/com/example/movies/MovieService.java
+++ src/main/java/com/example/movies/MovieService.java
@@ -1,8 +1,9 @@
 package com.example.movies;
 
 import java.util.List;
+import java.util.Optional;
 
-
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class MovieService {
     public List<Movie> allMovies(){
         System.out.println(movieRepository.findAll().toString());
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String id){
+        return movieRepository.findMovieByImdbId(id); 
     }
 }
